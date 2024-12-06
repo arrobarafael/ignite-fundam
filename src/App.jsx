57f79f9ee41aca087cd7fc4f38251fa82a145580
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-12-06 17:57:39 -0300

    Adicionado arquivo de estilização "escopada"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './styles.css';
 
 export function App() {
   return (
     <>
+      <Header />
       <Post
         author="Rafael P."
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis voluptatem asperiores eveniet ea blanditiis sint dicta amet in minima itaque atque est ullam ipsum deleniti nostrum, repellat incidunt commodi nulla!"
