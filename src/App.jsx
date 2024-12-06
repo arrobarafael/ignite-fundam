831ba97e827007e948e06151d720b2d0c7ba8f13
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-12-06 17:39:02 -0300

    Transmitir e ler propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import { Post } from './Post';
 export function App() {
   return (
     <>
-      <Post />
-      <h1>Olá</h1>
+      <Post
+        author="Rafael P."
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis voluptatem asperiores eveniet ea blanditiis sint dicta amet in minima itaque atque est ullam ipsum deleniti nostrum, repellat incidunt commodi nulla!"
+      />
+      <Post author="Roberto Souza" content="Novo post com conteúdo diverso" />
     </>
   );
 }
