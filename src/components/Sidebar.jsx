46ba342f55caff86f3bb60a1c0e55b7855c8d0eb
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-12-08 12:28:49 -0300

    Finalizando sidebar, com adição de avatar, icone através de componente novo instalado e estilização de botão

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,19 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <strong>Rafael</strong>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/14929854?v=4"
+        />
+        <strong>Rafael Pelegrini</strong>
         <span>Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
