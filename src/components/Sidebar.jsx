5c0950765eff54e8aa0775454c33e57d14ace3c8
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-12-08 17:08:54 -0300

    Implementado componente de avatar, junto a desestruturação de propriedades, valor padrão e estilização condicional

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,10 +12,12 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
+        {/* <img
           className={styles.avatar}
           src="https://avatars.githubusercontent.com/u/14929854?v=4"
-        />
+        /> */}
+        <Avatar src="https://github.com/arrobarafael.png" />
+
         <strong>Rafael Pelegrini</strong>
         <span>Developer</span>
       </div>
