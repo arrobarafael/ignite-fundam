607317a3d44d417f0e7cb6eefccfc7adde848d09
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-12-09 12:46:14 -0300

    Criando estrutura de adição de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,14 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
-  // const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR', {
-  //   day: '2-digit',
-  //   month: 'long',
-  //   hour: '2-digit',
-  //   minute: '2-digit',
-  // }).format(publishedAt);
+  const [comments, setComments] = useState([1, 2]);
+
   const publishedDateFormatted = format(
     publishedAt,
     "dd 'de' LLLL 'às' HH:mm'h'",
@@ -24,6 +21,13 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    console.log('handou');
+    setComments([...comments, comments.length + 1]);
+  }
 
   return (
     <article className={styles.post}>
@@ -58,7 +62,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea placeholder="Deixe um comentário" />
@@ -69,9 +73,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );
